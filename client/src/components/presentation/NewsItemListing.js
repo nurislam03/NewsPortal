6bae4a255637074ef2fac28eba001e62a671d23e
Nur Islam <nurislam0333@gmail.com>
2018-06-05 11:47:31 +0600

    added News | NewsItemDetail | NewsItemListing | etc

--- client/src/components/presentation/NewsItemListing.js
+++ client/src/components/presentation/NewsItemListing.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class NewsItemListing extends Component {
     render() {
         return (
-            <div>
-                <div><b>{this.props.title}</b></div>
+			<div>
+                <div><Link to={`/news/${this.props.id}`}><b>{this.props.title}</b></Link></div>
                 <div>{this.props.teaser}</div>
             </div>
         )
